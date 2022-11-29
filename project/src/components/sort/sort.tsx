8e3438c9f0e61befa7b0296d076a5783b7e3a37c
Ko0oT <serkokarjalo@gmail.com>
2022-11-29 18:41:00 +0300

    Добавит дропдаун типов сортировки и её изменение при выборе другого города

--- project/src/components/sort/sort.tsx
+++ project/src/components/sort/sort.tsx
@@ -1,5 +1,7 @@
 import { SortType } from '../../const';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useAppSelector } from '../../hooks';
+import { getCity } from '../../store/app-process/app-process-selectors';
 
 type SortProps = {
   sortOffers(action: keyof typeof SortType): void;
@@ -8,55 +10,72 @@ type SortProps = {
 function Sort({sortOffers}: SortProps) {
 
   const [title, setTitle] = useState('Popular');
+  const [dropdownShouldBeShown, setDropdownShouldBeShown] = useState<boolean>(false);
+
+  const city = useAppSelector(getCity);
+
+  useEffect(() => {
+    setTitle('Popular');
+    setDropdownShouldBeShown(false);
+  }, [city]);
 
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by  </span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={() => setDropdownShouldBeShown(true)}
+      >
         {title}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
-        <li
-          className="places__option places__option--active"
-          tabIndex={0}
-          onClick={(evt) => {
-            sortOffers(SortType.Default);
-            setTitle(evt.currentTarget.innerText);
-          }}
-        >
+      {dropdownShouldBeShown &&
+        <ul className="places__options places__options--custom places__options--opened">
+          <li
+            className="places__option places__option--active"
+            tabIndex={0}
+            onClick={(evt) => {
+              sortOffers(SortType.Default);
+              setTitle(evt.currentTarget.innerText);
+              setDropdownShouldBeShown(false);
+            }}
+          >
           Popular
-        </li>
-        <li
-          className="places__option"
-          tabIndex={0}
-          onClick={(evt) => {
-            sortOffers(SortType.PriceLowToHigh);
-            setTitle(evt.currentTarget.innerText);
-          }}
-        >Price: low to high
-        </li>
-        <li
-          className="places__option"
-          tabIndex={0}
-          onClick={(evt) => {
-            sortOffers(SortType.PriceHighToLow);
-            setTitle(evt.currentTarget.innerText);
-          }}
-        >Price: high to low
-        </li>
-        <li
-          className="places__option"
-          tabIndex={0}
-          onClick={(evt) => {
-            sortOffers(SortType.RatingHighToLow);
-            setTitle(evt.currentTarget.innerText);
-          }}
-        >Top rated first
-        </li>
-      </ul>
+          </li>
+          <li
+            className="places__option"
+            tabIndex={0}
+            onClick={(evt) => {
+              sortOffers(SortType.PriceLowToHigh);
+              setTitle(evt.currentTarget.innerText);
+              setDropdownShouldBeShown(false);
+            }}
+          >Price: low to high
+          </li>
+          <li
+            className="places__option"
+            tabIndex={0}
+            onClick={(evt) => {
+              sortOffers(SortType.PriceHighToLow);
+              setTitle(evt.currentTarget.innerText);
+              setDropdownShouldBeShown(false);
+            }}
+          >Price: high to low
+          </li>
+          <li
+            className="places__option"
+            tabIndex={0}
+            onClick={(evt) => {
+              sortOffers(SortType.RatingHighToLow);
+              setTitle(evt.currentTarget.innerText);
+              setDropdownShouldBeShown(false);
+            }}
+          >Top rated first
+          </li>
+        </ul>}
     </form>
   );
 }
